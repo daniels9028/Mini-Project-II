7dd46803b0bee1d1311b96ea708c860dcdcfe1f1
daniels9028 <daniels9028@student.untan.ac.id>
2024-12-08 14:20:31 +0700

    Login page with responsive design

---
+++ src/components/Input.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Input = ({ name, placeholder, type }) => {
+  return (
+    <div className="flex flex-col w-full gap-2 mt-3">
+      <h4 className="font-semibold tracking-wide capitalize">{name}</h4>
+      <input
+        type={type}
+        className="px-4 py-2 font-medium bg-gray-200 border-2 rounded-lg placeholder:italic placeholder:text-gray-500 focus:border-blue-600 focus:outline-none"
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default Input;
