109cbec4c645228aa028c668c55e9ee4570f279e
daniels9028 <daniels9028@student.untan.ac.id>
2024-12-08 14:24:55 +0700

    Add Register Route and Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
         <Route path="*" element={<PageNotFound />} />
       </Routes>
