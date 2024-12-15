85a9d4beb9225db61e463c4862983b1d4a613cce
daniels9028 <daniels9028@student.untan.ac.id>
2024-12-16 05:32:35 +0700

    Register with API

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import register from "../assets/register.jpg";
 import logo from "../assets/logo.png";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    confirmation_password: "",
+  });
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleRegister = async () => {
+    setLoading(true);
+    setError("");
+    console.log(form);
+
+    if (form.password !== form.confirmation_password) {
+      setError("Confirmation password does not match");
+      setLoading(false);
+      return;
+    }
+
+    const payload = {
+      email: form.email,
+      password: form.password,
+    };
+    try {
+      const res = await axios.post("https://reqres.in/api/register", payload);
+
+      setSuccess("Register user was successfully");
+
+      setTimeout(() => {
+        navigate("/");
+      }, 1000);
+    } catch (error) {
+      console.log(error);
+      setError(error.response.data.error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     document.title = "Register | Nmixx";
   }, []);
@@ -21,21 +69,48 @@ const Register = () => {
             </div>
             <p className="my-8 text-xl font-bold">Sign Up</p>
 
-            <Input name="email" placeholder="Input your email" type="text" />
+            {error && (
+              <p className="px-4 py-2 tracking-wide text-white capitalize bg-red-500 rounded-lg">
+                {error}
+              </p>
+            )}
+
+            {success && (
+              <p className="px-4 py-2 tracking-wide text-white capitalize bg-green-500 rounded-lg">
+                {success}
+              </p>
+            )}
+
+            <Input
+              name="email"
+              placeholder="Input your email"
+              type="text"
+              title="email"
+              onChange={handleChange}
+            />
 
             <Input
               name="password"
+              title="password"
               placeholder="Input your password"
               type="password"
+              onChange={handleChange}
             />
 
             <Input
-              name="confirmation password"
+              name="confirmation_password"
+              title="confirmation password"
               placeholder="Input your confirmation password"
               type="password"
+              onChange={handleChange}
             />
 
-            <Button name="Sign Up" className="font-bold bg-blue-500" />
+            <Button
+              name="Sign Up"
+              className="font-bold bg-blue-500 disabled:bg-blue-400"
+              onClick={handleRegister}
+              loading={loading}
+            />
 
             <div className="flex flex-col items-center justify-center gap-2 mt-6 font-medium text-center lg:flex-row">
               <p>Already have an account?</p>
