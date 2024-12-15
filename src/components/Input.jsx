5cd58a28616fefe69a7afdd71e665fb013348040
daniels9028 <daniels9028@student.untan.ac.id>
2024-12-16 05:18:19 +0700

    Login with Regres In API

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Input = ({ name, placeholder, type }) => {
+const Input = ({ name, placeholder, type, onChange }) => {
   return (
     <div className="flex flex-col w-full gap-2 mt-3">
       <h4 className="font-semibold tracking-wide capitalize">{name}</h4>
@@ -8,6 +8,8 @@ const Input = ({ name, placeholder, type }) => {
         type={type}
         className="px-4 py-2 font-medium bg-gray-200 border-2 rounded-lg placeholder:italic placeholder:text-gray-500 focus:border-blue-600 focus:outline-none"
         placeholder={placeholder}
+        onChange={onChange}
+        name={name}
       />
     </div>
   );
