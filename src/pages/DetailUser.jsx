aa8f1343a96084619bd7888f8378c9e7d21e953e
daniels9028 <daniels9028@student.untan.ac.id>
2024-12-16 18:29:44 +0700

    Navbar with logout function

--- src/pages/DetailUser.jsx
+++ src/pages/DetailUser.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const DetailUser = () => {
   const { id } = useParams();
@@ -21,31 +22,34 @@ const DetailUser = () => {
   }, []);
 
   return (
-    <div className="w-full p-5 bg-gray-200 lg:p-10">
-      <div className="container min-h-screen p-5 mx-auto bg-white rounded-xl lg:p-10">
-        <div className="flex flex-col items-center justify-center py-10">
-          <h3 className="text-2xl font-bold">Detail User</h3>
-          <div className="flex flex-col items-center justify-center w-full p-5 mt-10 overflow-hidden bg-gray-300 border border-gray-400 shadow-lg lg:w-1/2 rounded-xl">
-            <img
-              src={user?.avatar}
-              alt={user?.id}
-              className="object-cover w-32 border border-gray-400 rounded-full"
-            />
-            <p className="mt-4 text-xl font-bold text-center">
-              {user?.first_name} {user?.last_name}
-            </p>
-            <p className="mt-4 text-lg font-medium text-center">
-              {user?.email}
-            </p>
-            <Link to="/users">
-              <button className="px-6 py-2 mt-4 tracking-wider text-white transition-all bg-blue-600 rounded-full hover:bg-blue-900">
-                Kembali
-              </button>
-            </Link>
+    <>
+      <Navbar />
+      <div className="w-full p-5 mt-20 bg-gray-200 lg:p-10">
+        <div className="container min-h-screen p-5 mx-auto bg-white rounded-xl lg:p-10">
+          <div className="flex flex-col items-center justify-center py-10">
+            <h3 className="text-2xl font-bold">Detail User</h3>
+            <div className="flex flex-col items-center justify-center w-full p-5 mt-10 overflow-hidden bg-gray-300 border border-gray-400 shadow-lg lg:w-1/2 rounded-xl">
+              <img
+                src={user?.avatar}
+                alt={user?.id}
+                className="object-cover w-32 border border-gray-400 rounded-full"
+              />
+              <p className="mt-4 text-xl font-bold text-center">
+                {user?.first_name} {user?.last_name}
+              </p>
+              <p className="mt-4 text-lg font-medium text-center">
+                {user?.email}
+              </p>
+              <Link to="/users">
+                <button className="px-6 py-2 mt-4 tracking-wider text-white transition-all bg-blue-600 rounded-full hover:bg-blue-900">
+                  Kembali
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useState } from "react";
-import design from "../assets/design.png";
+import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
 
 const Users = () => {
@@ -50,55 +50,58 @@ const Users = () => {
   }, [page.page]);
 
   return (
-    <div className="w-full p-5 bg-gray-200 lg:p-10">
-      <div className="container min-h-screen p-5 mx-auto bg-white lg:p-10 rounded-xl">
-        <div className="flex flex-col items-center justify-center py-10">
-          <h3 className="text-2xl font-bold">List Users</h3>
-          <div className="grid grid-cols-1 gap-8 mt-10 lg:grid-cols-3 place-items-center">
-            {users.map((user) => (
-              <div
-                key={user.id}
-                className="bg-gradient-to-r from-gray-600 via-gray-700 to-gray-800 rounded-lg overflow-hidden h-[300px] w-[300px] flex flex-col items-center justify-center py-8 border border-black shadow-lg relative"
-              >
-                <div className="overflow-hidden border-2 border-white rounded-lg ">
-                  <img
-                    src={user.avatar}
-                    alt={user.id}
-                    className="object-cover w-20 h-20 transition-all duration-300 hover:scale-150"
-                  />
+    <>
+      <Navbar />
+      <div className="w-full p-5 mt-20 bg-gray-200 lg:p-10">
+        <div className="container min-h-screen p-5 mx-auto bg-white lg:p-10 rounded-xl">
+          <div className="flex flex-col items-center justify-center py-10">
+            <h3 className="text-2xl font-bold">List Users</h3>
+            <div className="grid grid-cols-1 gap-8 mt-10 lg:grid-cols-3 place-items-center">
+              {users.map((user) => (
+                <div
+                  key={user.id}
+                  className="bg-gradient-to-r from-gray-600 via-gray-700 to-gray-800 rounded-lg overflow-hidden h-[300px] w-[300px] flex flex-col items-center justify-center py-8 border border-black shadow-lg relative"
+                >
+                  <div className="overflow-hidden border-2 border-white rounded-lg ">
+                    <img
+                      src={user.avatar}
+                      alt={user.id}
+                      className="object-cover w-20 h-20 transition-all duration-300 hover:scale-150"
+                    />
+                  </div>
+                  <p className="mt-4 text-lg font-bold text-white">
+                    {user.first_name} {user.last_name}
+                  </p>
+                  <p className="mt-2 font-medium text-white">{user.email}</p>
+                  <Link to={`/detail_user/${user.id}`}>
+                    <button className="px-6 py-2 mt-6 font-semibold tracking-wider text-white transition-all duration-300 bg-black border border-black rounded-full hover:bg-white hover:text-black">
+                      Detail
+                    </button>
+                  </Link>
                 </div>
-                <p className="mt-4 text-lg font-bold text-white">
-                  {user.first_name} {user.last_name}
-                </p>
-                <p className="mt-2 font-medium text-white">{user.email}</p>
-                <Link to={`/detail_user/${user.id}`}>
-                  <button className="px-6 py-2 mt-6 font-semibold tracking-wider text-white transition-all duration-300 bg-black border border-black rounded-full hover:bg-white hover:text-black">
-                    Detail
-                  </button>
-                </Link>
-              </div>
-            ))}
-          </div>
-          <div className="flex flex-row items-center justify-center gap-10 mt-10">
-            <button
-              className="px-4 py-2 font-medium tracking-wider text-white bg-blue-500 rounded-full disabled:bg-blue-400 disabled:cursor-not-allowed"
-              disabled={page.page === 1}
-              onClick={handleBack}
-            >
-              Back
-            </button>
-            {page.page}
-            <button
-              className="px-4 py-2 font-medium tracking-wider text-white bg-blue-500 rounded-full disabled:bg-blue-400 disabled:cursor-not-allowed"
-              onClick={handleNext}
-              disabled={page.total_pages === page.page}
-            >
-              Next
-            </button>
+              ))}
+            </div>
+            <div className="flex flex-row items-center justify-center gap-10 mt-10">
+              <button
+                className="px-4 py-2 font-medium tracking-wider text-white bg-blue-500 rounded-full disabled:bg-blue-400 disabled:cursor-not-allowed"
+                disabled={page.page === 1}
+                onClick={handleBack}
+              >
+                Back
+              </button>
+              {page.page}
+              <button
+                className="px-4 py-2 font-medium tracking-wider text-white bg-blue-500 rounded-full disabled:bg-blue-400 disabled:cursor-not-allowed"
+                onClick={handleNext}
+                disabled={page.total_pages === page.page}
+              >
+                Next
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
