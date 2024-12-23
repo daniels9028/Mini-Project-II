45cfa0b78b8e3e457d8f4f9344af89dc82f81643
daniels9028 <daniels9028@student.untan.ac.id>
2024-12-23 09:24:57 +0700

    Dashboard Banner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo_navbar.png";
 import { Link, useNavigate } from "react-router-dom";
 import { FiAlignRight } from "react-icons/fi";
 import { FiX } from "react-icons/fi";
@@ -45,17 +45,21 @@ const Navbar = () => {
   };
   return (
     <div className="fixed top-0 left-0 z-50 w-full bg-white border-b-2 shadow-md border-slate-200">
-      <div className="container px-10 mx-auto">
+      <div className="container lg:px-10 px-5 mx-auto">
         <div className="flex flex-row items-center justify-between h-20">
           <div className="z-50 flex flex-row items-center justify-center gap-4">
-            <img src={logo} alt="logo" />
-            <Link to="/users" className="text-lg hover:text-blue-500">
-              <p className="text-xl font-bold tracking-widest">Nmixx</p>
-            </Link>
+            <img src={logo} alt="logo" className="w-32" />
           </div>
+          <ul className="lg:flex hidden flex-row justify-center items-center gap-8 font-bold cursor-pointer">
+            <li className="hover:text-[#2EBB77]">Home</li>
+            <li className="hover:text-[#2EBB77]">TeeSpace</li>
+            <li className="hover:text-[#2EBB77]">Shop</li>
+            <li className="hover:text-[#2EBB77]">Blog</li>
+            <li className="hover:text-[#2EBB77]">Pages</li>
+          </ul>
           <button
             onClick={token === null ? handleLogin : handleLogout}
-            className="hidden px-6 py-2 font-bold tracking-wider text-white transition-colors bg-red-500 rounded-full hover:bg-red-700 lg:block"
+            className="hidden px-6 py-2 text-sm font-bold tracking-wider text-white transition-colors bg-black rounded-full lg:block"
           >
             {token === null ? "Login" : "Logout"}
           </button>
@@ -71,10 +75,17 @@ const Navbar = () => {
             variants={menuVariants}
             className="fixed top-0 left-0 z-40 block w-full h-screen bg-gray-200 rounded-xl lg:hidden"
           >
-            <div className="flex flex-col items-center justify-center h-full gap-4">
+            <div className="flex flex-col items-center justify-center h-full my-10 gap-4">
+              <ul className="lg:hidden flex flex-col justify-center items-center gap-4 font-bold cursor-pointer">
+                <li className="hover:text-[#2EBB77]">Home</li>
+                <li className="hover:text-[#2EBB77]">TeeSpace</li>
+                <li className="hover:text-[#2EBB77]">Shop</li>
+                <li className="hover:text-[#2EBB77]">Blog</li>
+                <li className="hover:text-[#2EBB77]">Pages</li>
+              </ul>
               <button
                 onClick={token === null ? handleLogin : handleLogout}
-                className="px-6 py-2 font-bold tracking-wider text-white transition-colors bg-red-500 rounded-full hover:bg-red-700"
+                className="px-6 py-2 text-sm font-bold tracking-wider text-white bg-black rounded-full"
               >
                 {token === null ? "Login" : "Logout"}
               </button>
