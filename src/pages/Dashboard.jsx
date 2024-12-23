8b05346c03e8c854c9e1ae9b7b29eb708dfd8873
daniels9028 <daniels9028@student.untan.ac.id>
2024-12-23 10:34:35 +0700

    Custom tshirt

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,34 @@ import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
 import Main from "../components/Main";
+import Custom from "../components/Custom";
+
+import custom1 from "../assets/custom/custom1.png";
+import custom2 from "../assets/custom/custom2.png";
+import custom3 from "../assets/custom/custom3.png";
+
+const customs = [
+  {
+    id: 1,
+    title: "Add your shirt design",
+    subtitle:
+      '"Personalize Your Shirt: A Guide to Uploading Your Custom Design"',
+    image: custom1,
+  },
+  {
+    id: 2,
+    title: "Custom artwork &review",
+    subtitle:
+      '"Crafting Unique Designs: The Power of Custom Artwork and Feedback"',
+    image: custom2,
+  },
+  {
+    id: 3,
+    title: "Enjoy your product",
+    subtitle: '"Designed for You: Time to Enjoy Your Unique Creation"',
+    image: custom3,
+  },
+];
 
 const Dashboard = () => {
   useEffect(() => {
@@ -12,6 +40,7 @@ const Dashboard = () => {
       <Navbar />
       <Banner />
       <Main />
+      <Custom customs={customs} />
     </>
   );
 };
